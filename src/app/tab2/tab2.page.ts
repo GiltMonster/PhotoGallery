e87d449221392668c9aefd3325e50e830edff5e2
GiltMonster <lucassantoslukas2@gmail.com>
2024-07-24 20:47:04 -0300

    Feat: I added a function to delete the image

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from "../services/photo.service";
+import { ActionSheetController } from '@ionic/angular';
+import { UserPhoto } from '../interfaces/userPhoto';
 
 @Component({
   selector: 'app-tab2',
@@ -9,7 +11,8 @@ import { PhotoService } from "../services/photo.service";
 export class Tab2Page implements OnInit {
 
   constructor(
-    public photoService: PhotoService
+    public photoService: PhotoService,
+    public actionSheetController: ActionSheetController
   ) { }
 
   ngOnInit(): void {
@@ -18,6 +21,29 @@ export class Tab2Page implements OnInit {
 
   addPhotoToGallery() {
     this.photoService.addToGallery();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const action = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      },
+      {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet will automatically close
+        }
+      }]
+    });
+    await action.present();
   }
 
 }
